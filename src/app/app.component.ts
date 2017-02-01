a56d84cedfcc22e6725bef9ab7df244cc2d0ac0a
Patrick Goeler von Ravensburg <patrickvongoeler@gmail.com>
2017-02-02 00:11:21 +0100

    Added Websocket Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, HttpModule } from '@angular/http';
+import { WebsocketService } from './websocket.service';
+import { Subject, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  private socket: Subject<any>;
+  private message;
+  private message2;
+  i = 1;
+
+  constructor(private websocketService: WebsocketService, private http: Http) {
+    this.socket = websocketService.createWebsocketService();
+  }
+
+  ngOnInit() {
+    this.socket.subscribe(
+      message => this.message = JSON.parse(message.data)
+    );
+  }
+  displayInfo() {
+    for (var data in this.message) {
+      console.log(data);
+      console.log(this.message[data]);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { WebsocketService } from './websocket.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -14,7 +14,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [WebsocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
